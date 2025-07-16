75733e68074447676e65c1fa20732609e3a536a3
Pham Hoang <pham.hoang27022006@gmail.com>
2025-07-16 14:42:12 +0300

    Landing page support 3 languages

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,28 @@ import SignupPage from './auth/SignupPage';
 import HomePage from './home/HomePage';// Assuming this is the main component for the home page
 import SettingsPage from './settings/SettingsPage';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './services/i18n'; // Import your i18n configuration
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={
-        <PrivateRoute>
-          <HomePage />
-        </PrivateRoute>
-      } />
-      <Route path="/settings" element={
-        <PrivateRoute>
-          <SettingsPage />
-        </PrivateRoute>
-      } />
-    </Routes>
+    <I18nextProvider i18n={i18n}>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        } />
+        <Route path="/settings" element={
+          <PrivateRoute>
+            <SettingsPage />
+          </PrivateRoute>
+        } />
+      </Routes>
+    </I18nextProvider>
   );
 };
 
--- src/landing/Footer/Footer.tsx
+++ src/landing/Footer/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next'; // Add this import
 import styles from './Footer.module.css';
 
 const Footer: React.FC = () => {
   const navigate = useNavigate();
+  const { t } = useTranslation(); // Initialize useTranslation
 
   const handleGetStartedClick = () => {
     navigate('/login');
@@ -12,33 +14,33 @@ const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
-        <p>Ready to watch? Enter your email to create or restart your membership.</p>
+        <p>{t('footer.readyToWatch')}</p> {/* Use translation key */}
         <div className={styles.emailSignup}>
-          <input type="email" className={styles.emailInput} placeholder="Email address" />
+          <input type="email" className={styles.emailInput} placeholder={t('footer.emailPlaceholder')} /> {/* Use translation key */}
           <button type="button" className={styles.ctaButton} onClick={handleGetStartedClick}>
-            Get Started
+            {t('footer.getStarted')} {/* Use translation key */}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" role="img" viewBox="0 0 24 24" width="24" height="24" data-icon="ChevronRightStandard" aria-hidden="true">
-              <path fillRule="evenodd" clipRule="evenodd" d="M15.5859 12L8.29303 19.2928L9.70725 20.7071L17.7072 12.7071C17.8948 12.5195 18.0001 12.2652 18.0001 12C18.0001 11.7347 17.8948 11.4804 17.7072 11.2928L9.70724 3.29285L8.29303 4.70706L15.5859 12Z" fill="currentColor"></path>
+              <path fillRule="evenodd" clipRule="evenodd" d="M15.5859 12L8.29303 19.2928L9.70725 20.7071L17.7072 12.7071C17.8948 12.5195 18.0001 12.2652 18.0001 12C18.0001 11.7347 17.8948 11.4804 17.8948 11.2928L9.70724 3.29285L8.29303 4.70706L15.5859 12Z" fill="currentColor"></path>
             </svg>
           </button>
         </div>
       </div>
-      <p>Questions? Call 1-800-000-0000</p>
+      <p>{t('footer.questionsCall')}</p> {/* Use translation key */}
       <div className={styles.footerLinks}>
-        <a href="#">FAQ</a>
-        <a href="#">Help Centre</a>
-        <a href="#">Account</a>
-        <a href="#">Media Centre</a>
-        <a href="#">Investor Relations</a>
-        <a href="#">Jobs</a>
-        <a href="#">Ways to Watch</a>
-        <a href="#">Terms of Use</a>
-        <a href="#">Privacy</a>
-        <a href="#">Cookie Preferences</a>
-        <a href="#">Corporate Information</a>
-        <a href="#">Contact Us</a>
+        <a href="#">{t('footer.faq')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.helpCentre')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.account')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.mediaCentre')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.investorRelations')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.jobs')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.waysToWatch')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.termsOfUse')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.privacy')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.cookiePreferences')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.corporateInformation')}</a> {/* Use translation key */}
+        <a href="#">{t('footer.contactUs')}</a> {/* Use translation key */}
       </div>
-      <p className={styles.copyright}>&copy; 2024 Netflix, Inc.</p>
+      <p className={styles.copyright}>{t('footer.copyright')}</p> {/* Use translation key */}
     </footer>
   );
 };
