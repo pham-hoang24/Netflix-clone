2f76ac1fdaed23cb385d2c36cc039003b91c980a
Pham Hoang <pham.hoang27022006@gmail.com>
2025-07-21 13:12:04 +0300

    language set according to pref

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,14 +1,13 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: 'english',
+    lng: 'english', // Set a default language
     debug: true,
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
